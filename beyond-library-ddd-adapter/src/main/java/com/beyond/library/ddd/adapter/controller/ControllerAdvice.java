19f2a6958cff8156501dab888f5c181a36a84b1d
huangdebin <huangdebin@kintree.cn>
2022-11-15 09:54:27 +0800

    feat: add ControllerAdvice

---
+++ beyond-library-ddd-adapter/src/main/java/com/beyond/library/ddd/adapter/controller/ControllerAdvice.java
@@ -0,0 +1,19 @@
+package com.beyond.library.ddd.adapter.controller;
+
+import com.beyond.library.exception.BizException;
+import com.beyond.library.result.Result;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author beyond
+ * @since 2022/11/15
+ */
+@RestControllerAdvice
+public class ControllerAdvice {
+
+    @ExceptionHandler(BizException.class)
+    public Result<Void> handler(BizException e) {
+        return Result.error(e.getCode(), e.getMessage());
+    }
+}
